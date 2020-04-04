bb56aad971134a2bf782e2b7233bd7bb49f0abe5
mehdi.rachico@insat.u-carthage.tn <mehdi.rachico@insat.u-carthage.tn>
2020-04-04 19:05:44 +0200

    switched id to uuid

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,22 +7,26 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    
+
     public function register(Request $request)
     {
         $validatedData =$request ->validate ([
 
             'name'=>'required|max:55' ,
             'email'=>'email|required',
-            'password'=> 'required|confirmed',
-            
+            'password'=> 'required',
+
         ]);
-    
+
     $user = User::create($validatedData);
     $accessToken=$user->createToken('authToken')->accessToken;
-    return response (['user'=>$user,'access_token'=>$accessToken]);
+    return response ([
+        'user'=>$user,
+        'access_token'=>$accessToken,
+        'message' => 'Successfully created user!'
+    ]);
 
     }
 
-   
+
 }

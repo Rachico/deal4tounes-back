98bda181bf2c2d9123942695c1a2fde5d95cc675
eyaab <abdelmoulaeya@yahoo.fr>
2020-04-07 00:52:36 +0100

    insert ver7 into project

--- backend ver 7/laravel/app/Http/Controllers/Api/AuthController.php
+++ backend ver 7/laravel/app/Http/Controllers/Api/AuthController.php
@@ -2,31 +2,25 @@
 
 namespace App\Http\Controllers\Api;
 use App\user ;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-
+  
     public function register(Request $request)
     {
         $validatedData =$request ->validate ([
 
             'name'=>'required|max:55' ,
             'email'=>'email|required|unique:users',
-            'password'=> 'required',
-
+            'password'=> 'required|confirmed',
+            
         ]);
-
-        $validatedData['password']= bcrypt($request->password);
-
+    $validatedData['password']= bcrypt($request->password);
     $user = User::create($validatedData);
     $accessToken=$user->createToken('authToken')->accessToken;
-    return response ([
-        'user'=>$user,
-        'access_token'=>$accessToken,
-        'message' => 'Successfully created user!'
-    ]);
+    return response (['user'=>$user,'access_token'=>$accessToken]);
 
     }
     public function login(Request $request){
@@ -42,5 +36,4 @@ class AuthController extends Controller
         return response (['user'=>auth()->user(),'access_token'=>$accessToken]);
     
     }
-
 }
